b27f66822cc3d8e308a65bf4c95509a4dc369964
Rayn Yorobe <raynyorobe@gmail.com>
2026-02-22 00:00:48 -0600

    feat: enable experimental Nix features and update flake dependencies

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    gazelle.url = "github:Zeus-Deus/gazelle-tui";
 
     home-manager = {
       url = "github:nix-community/home-manager";
--- modules/system/boot/boot.nix
+++ modules/system/boot/boot.nix
@@ -5,18 +5,15 @@
     limine = {
       enable = true;
       efiSupport = true;
-    };
 
-    extraConfig = ''
-        # Tell Limine to look at the root of the boot partition for the image
-        wallpaper=boot:///boot-bg.png
-        
-        # Optional: Darken the background slightly so the white text is readable
-        term_background=00000000
-        term_background_style=tiled
-        
-        # Optional: Add a margin so the text doesn't touch the edge of the screen
-        term_margin=50
+      style = {
+        wallpapers = [ ./boot-bg.png ];
+        wallpaperStyle = "stretched";
+      };
+  
+      extraConfig = ''
+        term_background: 80000000
+        term_margin: 50
       '';
     };
 
