3c49ec0bbd03a1132be40d65e4fa49c22910d19b
Rayn Yorobe <raynyorobe@gmail.com>
2026-02-22 01:26:00 -0600

    feat: update key bindings for Hyprland window manager

--- modules/hyprland/binds.nix
+++ modules/hyprland/binds.nix
@@ -2,12 +2,13 @@
   wayland.windowManager.hyprland.settings = {
     "$mainMod" = "SUPER";
     bind = [
-      "$mainMod, Q, exec, $terminal"
-      "$mainMod, C, killactive,"
+      "$mainMod, RETURN, exec, $terminal"
+      "$mainMod, W, killactive,"
       "$mainMod, M, exit,"
-      "$mainMod, E, exec, $fileManager"
+      "$mainMod SHIFT, F, exec, $fileManager"
       "$mainMod, V, togglefloating,"
-      "$mainMod, R, exec, $menu"
+      "$mainMod, F, fullscreen, 0,"
+      "$mainMod, SPACE, exec, $menu"
       "$mainMod, P, pseudo," # dwindle
       "$mainMod, J, togglesplit," # dwindle
 
@@ -21,7 +22,35 @@
       "$mainMod, 1, workspace, 1"
       "$mainMod, 2, workspace, 2"
       "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+
+      # Move active window to a workspace with mainMod + Shift + [0-9]
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+      # Special Workspace
+      "$mainMod, TAB, togglespecialworkspace, magic"
+      "$mainMod SHIFT, TAB, movespecialworkspace, magic"
+
+      # Scroll through workspaces with mainMod + mouse wheel
+      "$mainMod, mouse_down, workspace, e+1"
+      "$mainMod, mouse_up, workspace, e-1"
     ];
+
     # Mouse binds
     bindm = [
       "$mainMod, mouse:272, movewindow"
